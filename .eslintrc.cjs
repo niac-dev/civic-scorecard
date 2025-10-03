b5b31bf2e945d779312b2b7cecd4db0f6efc60a0
Jamal Abdi <jamalabdi@mac.myfiosgateway.com>
2025-10-03 10:10:46 -0400

    chore: relax lint rules for deployment

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+// .eslintrc.cjs
+module.exports = {
+  extends: ["next", "next/core-web-vitals"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }],
+    "react-hooks/exhaustive-deps": "warn",
+  },
+};
